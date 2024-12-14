e2bde949b42ef54d8bab7c0ac018681fa55ce5a6
aesophsu <41796611+aesophsu@users.noreply.github.com>
2024-12-14 18:35:41 +0800

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "basic jacky's flake";
+
+  # the nixConfig here only affects the flake itself, not the system configuration!
+  nixConfig = {
+    # substituers will be appended to the default substituters when fetching packages
+    # nix com    extra-substituters = [munity's cache server
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
+  inputs = { 
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  };
+
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    ...
+  }: {
+    nixosConfigurations = {
+      nixos-homelab = let
+        username = "jacky";
+        specialArgs = { inherit username;};
+      in
+        nixpkgs.lib.nixosSystem {
+          inherit specialArgs;
+          system = "x86_64-linux";
+
+          modules = [  
+            ./hosts/nixos-homelab
+            ./users/${username}/nixos.nix
+          ];
+        };
+
+    };
+  };
+}
