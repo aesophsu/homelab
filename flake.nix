9f5ff8010504f053cd2cbb757bf9add6bebbf2d2
aesophsu <41796611+aesophsu@users.noreply.github.com>
2024-12-24 21:18:00 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -14,12 +14,15 @@
   };
 
   inputs = { 
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs @ {
     self,
     nixpkgs,
+    home-manager,
     ...
   }: {
     nixosConfigurations = {
@@ -34,6 +37,14 @@
           modules = [  
             ./hosts/nixos-homelab
             ./users/${username}/nixos.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+
+              home-manager.extraSpecialArgs = inputs // specialArgs;
+              home-manager.users.${username} = import ./users/${username}/home.nix;
+            }
           ];
         };
 
