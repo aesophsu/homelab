d39a7abccc32c9d52c4e653965091ce894631c6e
aesophsu <41796611+aesophsu@users.noreply.github.com>
2025-11-03 20:35:08 +0800

    Delete users/jacky directory

--- users/jacky/nixos.nix
+++
@@ -1,7 +0,0 @@
-{
-  users.users.jacky = {
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAVxgOWNu39zQHEvzg+l48kCePYAeuVRIY4Dm5MNy6H4 aesophsu@gmail.com"
-    ];
-  };
-}
