3a1177df275770b31a40fa3490453824ccd193bd
aesophsu <41796611+aesophsu@users.noreply.github.com>
2025-11-03 20:33:23 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,53 +1,19 @@
+# flake.nix
 {
-  description = "basic jacky's flake";
+  description = "NixOS All-in-One Family Server Configuration";
 
-  # the nixConfig here only affects the flake itself, not the system configuration!
-  nixConfig = {
-    # substituers will be appended to the default substituters when fetching packages
-    # nix com    extra-substituters = [munity's cache server
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    sops-nix.url = "github:Mic92/sops-nix";
   };
 
-  inputs = { 
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  outputs = inputs @ {
-    self,
-    nixpkgs,
-    home-manager,
-    ...
-  }: {
-    nixosConfigurations = {
-      nixos-homelab = let
-        username = "jacky";
-        specialArgs = { inherit username;};
-      in
-        nixpkgs.lib.nixosSystem {
-          inherit specialArgs;
-          system = "x86_64-linux";
-
-          modules = [  
-            ./hosts/nixos-homelab
-            ./users/${username}/nixos.nix
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-
-              home-manager.extraSpecialArgs = inputs // specialArgs;
-              home-manager.users.${username} = import ./users/${username}/home.nix;
-            }
-          ];
-        };
-
+  outputs = { self, nixpkgs, sops-nix }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        sops-nix.nixosModules.sops
+        ./configuration.nix
+      ];
     };
   };
 }
